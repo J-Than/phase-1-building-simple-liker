00d864c7812d59bb17714fa04213c07bf533a5f2
Jarod Wilson <jaroddwilson@gmail.com>
2021-10-31 13:19:10 -0400

    commit tree 40a1b1eca5bd067d91b3e5b2fe433c4cd83063c8

--- main.js
+++ main.js
@@ -5,7 +5,30 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+document.querySelectorAll('.like-glyph').forEach(button => {
+  button.addEventListener('click', handleLike)
+})
 
+function handleLike(e) {
+  const serverAnswer = mimicServerCall();
+  serverAnswer.then((reply) => {
+    const obj = e.target;
+    if(obj.textContent === EMPTY_HEART) {
+      obj.setAttribute('class', 'like-glyph activated-heart');
+      obj.textContent = FULL_HEART;
+    } else {
+      obj.setAttribute('class', 'like-glyph');
+      obj.textContent = EMPTY_HEART;
+    }
+  })
+  serverAnswer.catch((reply) => {
+    document.getElementById('modal').removeAttribute('class');
+    document.getElementById('modal-message').textContent = reply;
+    setTimeout(() => {
+      document.getElementById('modal').setAttribute('class', 'hidden');
+    }, 3000)
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
